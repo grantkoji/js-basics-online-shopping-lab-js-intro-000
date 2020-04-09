5e98650383568e1f139e0c3c9dfa6ee0fad51a6a
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-09 00:05:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,18 +10,45 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(1+(Math.random()*100));
+  var itemAndPrice = `{ itemName: ${item}, itemPrice: ${price} }`;
+  cart.push(itemAndPrice);
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    return 'Your shopping cart is empty.'
+  }
+  else {
+    var cartview = 'In your cart, you have'
+    if (cart.length === 1) {
+      return `${cartview} ${cart.itemName[0]} at $${cart.itemPrice[0]}.`
+    }
+    else {
+      for (i = 0; i < (cart.length - 1); i++) {
+      cartview = `${cartview} ${cart.itemName[i]} at $${cart.itemPrice[i]},`
+      }
+      return `${cartview} and ${cart.itemName[(cart.length - 1)]} at $${cart.itemPrice[(cart.length - 1)]}.`
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  if (cart.length === 0) {
+    return 0
+  }
+  else {
+    var total = 0;
+    for (i = 0; i < cart.length; i++ ) {
+      total = total + cart.itemPrice[i];
+    }
+    return total;
+  }
 }
 
 function removeFromCart(item) {
+
   // write your code here
 }
 
